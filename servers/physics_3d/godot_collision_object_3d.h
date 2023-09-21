012202c6e39bc08fee884af7d66678aaa917e5d1
Tiger Jove <tiger@jupiter.blue>
2023-09-21 18:22:07 +0200

    Proof of Concept implementation for parallel Dimensions in physics, plus a null-collision-dimension
    Signed-off-by: Tiger Jove <tiger@jupiter.blue>

--- servers/physics_3d/godot_collision_object_3d.h
+++ servers/physics_3d/godot_collision_object_3d.h
@@ -45,6 +45,8 @@
 
 class GodotSpace3D;
 
+const uint32_t COLLISION_WORLD = (65535<<16);
+
 class GodotCollisionObject3D : public GodotShapeOwner3D {
 public:
 	enum Type {
@@ -173,12 +175,16 @@ public:
 	}
 	_FORCE_INLINE_ real_t get_collision_priority() const { return collision_priority; }
 
+	_FORCE_INLINE_ bool shares_world(const GodotCollisionObject3D *p_other) const {
+		return (COLLISION_WORLD & collision_layer) && ((COLLISION_WORLD & collision_layer) == (COLLISION_WORLD & p_other->collision_layer));
+	}
+
 	_FORCE_INLINE_ bool collides_with(GodotCollisionObject3D *p_other) const {
-		return p_other->collision_layer & collision_mask;
+		return shares_world(p_other) && (p_other->collision_layer & collision_mask);
 	}
 
 	_FORCE_INLINE_ bool interacts_with(const GodotCollisionObject3D *p_other) const {
-		return collision_layer & p_other->collision_mask || p_other->collision_layer & collision_mask;
+		return shares_world(p_other) && (collision_layer & p_other->collision_mask || p_other->collision_layer & collision_mask);
 	}
 
 	void remove_shape(GodotShape3D *p_shape) override;
